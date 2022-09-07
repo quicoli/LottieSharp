c94e12d28d0d551b80b997c6e37cfa4be4ba888d
quicoli <pauloquicoli@gmail.com>
2022-09-07 21:40:40 +0100

    Implemented INotifyPropertyChanged

--- src/LottieSharp/LottieSharp.WPF/AnimationInfo.cs
+++ src/LottieSharp/LottieSharp.WPF/AnimationInfo.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace LottieSharp.WPF
 {
-    public class AnimationInfo
+    public class AnimationInfo : INotifyPropertyChanged
     {
+        private string version;
+        private TimeSpan duration;
+        private double fps;
+        private double inPoint;
+        private double outPoint;
+
         public AnimationInfo(string version, TimeSpan duration, double fps, double inPoint, double outPoint)
         {
             Version = version;
@@ -13,10 +21,59 @@ namespace LottieSharp.WPF
             OutPoint = outPoint;
         }
 
-        public string Version { get; private set; }
-        public TimeSpan Duration { get; private set; }
-        public double Fps { get; private set; }
-        public double InPoint { get; private set; }
-        public double OutPoint { get; private set; }
+        public string Version
+        {
+            get => version; private
+            set
+            {
+                version = value;
+                OnPropertyChanged();
+            }
+        }
+        public TimeSpan Duration
+        {
+            get => duration;
+            private set
+            {
+                duration = value;
+                OnPropertyChanged();
+            }
+        }
+        public double Fps
+        {
+            get => fps;
+            private set
+            {
+                fps = value;
+                OnPropertyChanged();
+            }
+        }
+        public double InPoint
+        {
+            get => inPoint;
+            private set
+            {
+                inPoint = value;
+                OnPropertyChanged();
+            }
+        }
+        public double OutPoint
+        {
+            get => outPoint;
+            private set
+            {
+                outPoint = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
